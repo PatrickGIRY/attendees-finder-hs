71ab68dc8474f0cc9d7968a8db6afb84cb63e9d6
patrickgiry <patrickgiry@gmail.com>
2025-08-01 00:15:09 +0200

    Add unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+import AttendeesFinder(findByInfixOfFirstName, Attendee(firstName, Attendee))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    let attendees = [Attendee { firstName = "Marc" }, 
+                     Attendee { firstName = "Christelle" },
+                     Attendee { firstName = "Christophe" }]
+
+    describe "The attendees finder should return" $ do
+        it "an empty result when no attendee first name matches the query string" $ do
+            findByInfixOfFirstName "Paul" attendees `shouldBe` []
+
+        it "one result when only one attendee first name matches the query string" $ do
+            findByInfixOfFirstName "Marc" attendees `shouldBe` [Attendee {firstName = "Marc"}]
+
+        it "all the results when many attendees first names match the query string" $ do
+            findByInfixOfFirstName "Chri" attendees `shouldBe` [Attendee {firstName = "Christelle"}, Attendee {firstName = "Christophe"}]
